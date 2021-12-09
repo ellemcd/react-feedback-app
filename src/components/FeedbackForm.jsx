60ef16fcdd5da660e0756382c6d9ec0553b1f6dc
Elle <eleonormcdowell@gmail.com>
2021-12-09 18:17:41 +0100

    Adding funcionallity to submit a review

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,7 +4,7 @@ import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
 
 
-function FeedbackForm() {
+function FeedbackForm({ handleAddFeedback }) {
 
 	const [text, setText] = useState('')
 	const [rating, setRating] = useState(10)
@@ -28,9 +28,22 @@ function FeedbackForm() {
 		setText(e.target.value)
 	}
 
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		if(text.trim().length > 10) {
+			const newFeedback = {
+				text: text,
+				rating: rating,
+			}
+
+			handleAddFeedback(newFeedback);
+			setText('')
+		}
+	}
+
 	return (
 		<Card>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<h2>How would you rate your service with us?</h2>
 				<RatingSelect select={(rating) => setRating(rating)} />
 				<div className="input-group">
