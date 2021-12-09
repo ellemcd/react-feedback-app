1e5972f3208554bf5ae9076fed5c18fd2c10c897
Elle <eleonormcdowell@gmail.com>
2021-12-09 17:58:13 +0100

    adding logic to ratings

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,30 @@
 import {useState} from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
+
 
 function FeedbackForm() {
 
 	const [text, setText] = useState('')
+	const [rating, setRating] = useState(10)
+
+	const [btnDisabled, setBtnDisabled] = useState(true)
+	const [message, setMessage] = useState('')
 
 	const handleTextChange = (e) => {
+
+		if(text === '') {
+			setBtnDisabled(true)
+			setMessage(null)
+		} else if(text !== '' && text.trim().length <= 10){
+			setMessage('Text must be at least 10 characters')
+			setBtnDisabled(true)
+		} else {
+			setMessage(null)
+			setBtnDisabled(false)
+		}
+
 		setText(e.target.value)
 	}
 
@@ -13,6 +32,7 @@ function FeedbackForm() {
 		<Card>
 			<form>
 				<h2>How would you rate your service with us?</h2>
+				<RatingSelect select={(rating) => setRating(rating)} />
 				<div className="input-group">
 					<input 
 					onChange={handleTextChange}
@@ -20,8 +40,9 @@ function FeedbackForm() {
 					placeholder="Write a review" 
 					value={text}
 					 />
-					<button type="submit">Send</button>
+					<Button type="submit" isDisabled={btnDisabled}>Send</Button>
 				</div>
+				{message && <div className="message">{message}</div>}
 			</form>
 		</Card>
 	)
