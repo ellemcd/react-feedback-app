ceae56cde15643e876c14f898328768660589c6e
Elle <eleonormcdowell@gmail.com>
2021-12-10 16:15:13 +0100

    Added Router and added a About Page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import Card from "../components/shared/Card"
+
+function AboutPage() {
+	return (
+		<Card>
+			<div className="about">
+				<h1>About this Project</h1>
+				<p>This is a React app to lave feedback for a product or service.</p>
+			</div>
+			<p>Version: 1.0.0</p>
+		</Card>
+	)
+}
+
+export default AboutPage
